100608d934324b8742ed2e64b50d125f3e495e59
Xavier <xavier@kalyxoweb.com>
2026-02-10 14:33:24 -0800

    fix build issue

--- src/lib/db/index.ts
+++ src/lib/db/index.ts
@@ -1,7 +1,15 @@
 import { neon } from "@neondatabase/serverless";
-import { drizzle } from "drizzle-orm/neon-http";
+import { drizzle, type NeonHttpDatabase } from "drizzle-orm/neon-http";
 import * as schema from "./schema";
 
-const sql = neon(process.env.DATABASE_URL!);
+let _db: NeonHttpDatabase<typeof schema>;
 
-export const db = drizzle(sql, { schema });
+export const db = new Proxy({} as NeonHttpDatabase<typeof schema>, {
+  get(_target, prop) {
+    if (!_db) {
+      const sql = neon(process.env.DATABASE_URL!);
+      _db = drizzle(sql, { schema });
+    }
+    return Reflect.get(_db, prop);
+  },
+});
